362e5eb6e8b2d9e78ee8ea0127359bc4850b9a8e
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2018-11-02 23:50:32 +0900

    회원가입 - ID중복체크

--- ToPM/app/src/main/java/com/example/knk/topm/JoinActivity.java
+++ ToPM/app/src/main/java/com/example/knk/topm/JoinActivity.java
@@ -1,14 +1,25 @@
 package com.example.knk.topm;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.knk.topm.Object.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class JoinActivity extends AppCompatActivity {
 
@@ -37,14 +48,58 @@ public class JoinActivity extends AppCompatActivity {
     //ID 중복검사 버튼 클릭이벤트 함수
     public void idCheckClick(View view) {
         input_id = ((EditText)findViewById(R.id.input_id)).getText().toString();
-        //if(rootReference.equals(input_id))
+        Log.v("Input_ID",input_id);
+
+        rootReference.addValueEventListener(new ValueEventListener() {
+            boolean isValidID = true;
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data : dataSnapshot.getChildren()){
+                    if(data.getKey().equals(input_id)) {
+                        Log.d("DataSnapshot", "onValueAdded:" + data);
+                        isValidID = false;
+                        break;
+                    }
+                }
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(JoinActivity.this);
+                if(isValidID){
+                    joinBtn.setEnabled(true);
+                    alertBuilder.setTitle("아이디 승인!");
+                    alertBuilder.setMessage(input_id+"(은)는 사용가능한 아이디입니다.");
+                    alertBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    }).show();
+                }
+                else{
+                    joinBtn.setEnabled(false);
+                    alertBuilder.setTitle("아이디 중복!");
+                    alertBuilder.setMessage(input_id+"(은)는 이미 존재하는 아이디입니다. 다시 시도해주세요.");
+                    alertBuilder.setNeutralButton("close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    }).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
     }
 
     //회원 가입 완료 버튼 클릭이벤트 함수
     public void completeClick(View view) {
         //ID 중복검사 버튼을 반드시 누르고 그 검사에 통과한 후에 활성화돼야 함.
-
+        input_id = ((EditText)findViewById(R.id.input_id)).getText().toString();
         input_pw = ((EditText)findViewById(R.id.input_pw)).getText().toString();
         input_birth = ((EditText)findViewById(R.id.input_birth)).getText().toString();
         input_name = ((EditText)findViewById(R.id.input_name)).getText().toString();
