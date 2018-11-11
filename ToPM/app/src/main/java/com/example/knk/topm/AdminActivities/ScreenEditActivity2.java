0d9b0cda23d8e6294049c3d654c7ede3c2ec06d1
Hyejin Hong <hhj732@naver.com>
2018-11-11 20:44:09 +0900

    Update ToPM/app/src/main/java/com/example/knk/topm/AdminActivities/ScreenEditActivity2.java

--- ToPM/app/src/main/java/com/example/knk/topm/AdminActivities/ScreenEditActivity2.java
+++ ToPM/app/src/main/java/com/example/knk/topm/AdminActivities/ScreenEditActivity2.java
@@ -1,7 +1,6 @@
 package com.example.knk.topm.AdminActivities;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
