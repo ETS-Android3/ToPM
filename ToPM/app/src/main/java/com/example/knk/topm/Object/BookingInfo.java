3643f4549ea0e7158b06bfa91f89a5717ba2c54d
Hyejin Hong <hhj732@naver.com>
2018-11-15 16:39:11 +0900

    나의 예매목록 출력됩니다
    자리 행 알파벳으로 바꾸는 거는 생각나면 할게요

--- ToPM/app/src/main/java/com/example/knk/topm/Object/BookingInfo.java
+++ ToPM/app/src/main/java/com/example/knk/topm/Object/BookingInfo.java
@@ -1,6 +1,7 @@
 package com.example.knk.topm.Object;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class BookingInfo {
 //    User user;
@@ -9,12 +10,35 @@ public class BookingInfo {
     String scheduleKey;             // 예매한 스케줄 key
     int personnel;                  // 인원
     ArrayList<String> bookedSeats;  // 예매한 자리 번호 저장 (버튼 ID)
+    Date screeningDate;             // 상영 시간
+    String title;                   // 영화 제목
 
-    public BookingInfo(String userID, String scheduleKey, int personnel, ArrayList<String> bookedSeats) {
+    public BookingInfo(String userID, String scheduleKey, int personnel, ArrayList<String> bookedSeats, Date screeningDate, String title) {
         this.userID = userID;
         this.scheduleKey = scheduleKey;
         this.personnel = personnel;
         this.bookedSeats = bookedSeats;
+        this.screeningDate = screeningDate;
+        this.title = title;
+    }
+
+    public Date getScreeningDate() {
+        return screeningDate;
+    }
+
+    public void setScreeningDate(Date screeningDate) {
+        this.screeningDate = screeningDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BookingInfo() {
     }
 
     public String getUserID() {
